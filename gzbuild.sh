b776e38bc76361139a5e8b40f9eb7b0f3c889c60
Riyadh <82821526+Riyadh-kh@users.noreply.github.com>
2025-07-11 14:16:15 +0400

    added san fixed origin mesh
    now using the new san with fixed origin

--- gzbuild.sh
+++ gzbuild.sh
@@ -14,7 +14,8 @@ cd ..
 export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=$(pwd)/build
 
 cd "$WORKSPACE" || exit
-colcon build
+source /opt/ros/humble/setup.bash
+colcon build --packages-ignore race_plan_control
 source install/setup.bash
 
 ros2 launch gen0_main spawn.launch.py world:=san_full actors_scenario:=walking_actors ground_truth_localization:=true
