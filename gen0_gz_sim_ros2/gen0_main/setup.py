0c681bd7ba4b32c2e43ab84d55db33fb29ec7d9b
Syed Abbas Hussain <s.abbashussain@outlook.com>
2025-09-03 13:11:57 +0400

    fixed san mesh

--- gen0_gz_sim_ros2/gen0_main/setup.py
+++ gen0_gz_sim_ros2/gen0_main/setup.py
@@ -47,7 +47,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='av-lab',
-    maintainer_email='alkharrat.riyadh@gmail.com',
+    maintainer_email='syed.ahussain@ku.ac.ae',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
