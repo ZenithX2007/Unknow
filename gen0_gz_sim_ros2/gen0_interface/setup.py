959cb878cf8fc38be3140d621418b7d993a8032b
Syed Abbas Hussain <s.abbashussain@outlook.com>
2025-09-08 10:32:46 +0400

    gen0_interface: switch to ament_python pkg

---
+++ gen0_gz_sim_ros2/gen0_interface/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'gen0_interface'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='av-lab',
+    maintainer_email='syed.ahussain@ku.ac.ae',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'cmdvel_to_vehicle = gen0_interface.cmdvel_to_vehicle:main',
+        ],
+    },
+)
